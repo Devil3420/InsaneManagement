dab4396cf02d4044e249603a9cbaee9beb6c7dd2
Devil3420 <105910403+Devil3420@users.noreply.github.com>
2022-06-21 10:24:45 +0530

    insane

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m InsaneRobotRobot
+web: python3 -m InsaneRobotRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,6 @@
+# why I'm changing this 
+
+@echo off
+TITLE InsaneRobotRobot Chan
+:: Enables virtual env mode and then starts Insane
+env\scripts\activate.bat && py -m InsaneRobotRobot
