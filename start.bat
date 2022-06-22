6893855153c488c0b6f6deba7d4c60ff6442bc7b
Devil3420 <105910403+Devil3420@users.noreply.github.com>
2022-06-22 10:59:31 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 # why I'm changing this 
 
 @echo off
-TITLE InsaneRobotRobot Chan
+TITLE InsaneRobot Chan
 :: Enables virtual env mode and then starts Insane
-env\scripts\activate.bat && py -m InsaneRobotRobot
+env\scripts\activate.bat && py -m InsaneRobot
