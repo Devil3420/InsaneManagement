49ab2330b43688183262e7b3bed3f086330c7c28
Devil3420 <105910403+Devil3420@users.noreply.github.com>
2022-06-26 18:44:57 +0530

    Update __init__.py

--- InsaneRobot/__init__.py
+++ InsaneRobot/__init__.py
@@ -227,7 +227,11 @@ pgram = Client("InsaneRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
-
+try:
+    ubot2.start()
+except BaseException:
+    print("Userbot Error! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
 
 pbot = Client(
     ":memory:",
