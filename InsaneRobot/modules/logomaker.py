7e4537db95bd0a09cbff30a150f8d50e9293fb13
Devil3420 <105910403+Devil3420@users.noreply.github.com>
2022-07-06 12:47:31 +0530

    Update logomaker.py

--- InsaneRobot/modules/logomaker.py
+++ InsaneRobot/modules/logomaker.py
@@ -11,9 +11,8 @@ import random
 import glob
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterPhotos
-from InsaneRobot.events import register
-from InsaneRobot import telethn as tbot, ubot2
-
+from IronMenRobot.events import register
+from IronMenRobot import telethn as tbot, ubot2
 
 def mediainfo(media):
     xx = str((str(media)).split("(", maxsplit=1)[0])
@@ -50,7 +49,7 @@ def mediainfo(media):
     return m
 
 
-@register(pattern="^/logo ?(.*)")
+@register(pattern="^/blogo ?(.*)")
 async def logo_gen(event):
     xx = await event.reply("`Preparing your logo...`")
     name = event.pattern_match.group(1)
@@ -71,23 +70,23 @@ async def logo_gen(event):
     else:
         pics = []
         async for i in ubot2.iter_messages(
-            "@KenLogopack", filter=InputMessagesFilterPhotos
+            "@alexalogopack", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./InsaneRobot/resources/fonts/*")
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
         async for i in ubot2.iter_messages(
-            "@KenLogopack", filter=InputMessagesFilterPhotos
+            "@alexalogopack", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./InsaneRobot/resources/fonts/*")
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 120
@@ -121,7 +120,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Insane Help](https://t.me/Insane_Help)",
+            caption="ᴛʜɪs ʟᴏɢᴏ ɪs ʙᴀsᴇᴅ ᴏɴ ᴜʙ ᴀɴᴅ ᴍᴀᴅᴇ ʙʏ [IronMen ʜᴇʟᴘ](https://t.me/IronMenfed)",
             force_document=False,
         )
         os.remove(flnme)
@@ -129,11 +128,11 @@ async def logo_gen(event):
     if os.path.exists(bg_):
         os.remove(bg_) 
     if os.path.exists(font_):
-        if not font_.startswith("./InsaneRobot/resources/fonts"):
+        if not font_.startswith("./IronMenRobot/resources/fonts"):
             os.remove(font_)
 
 
-@register(pattern="^/wlogo ?(.*)")
+@register(pattern="^/bwlogo ?(.*)")
 async def logo_(event):
     xx = await event.reply("`Preparing your logo...`")
     name = event.pattern_match.group(1)
@@ -154,23 +153,23 @@ async def logo_(event):
     else:
         pics = []
         async for i in ubot2.iter_messages(
-            "@kenlogopack", filter=InputMessagesFilterPhotos
+            "@alexalogopack", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./InsaneRobot/resources/fonts/*")
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
         async for i in ubot2.iter_messages(
-            "@kenlogopack", filter=InputMessagesFilterPhotos
+            "@alexalogopack", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./InsaneRobot/resources/fonts/*")
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 105
@@ -204,7 +203,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Mayank](https://t.me/always_hungry365)",
+            caption="ᴛʜɪs ʟᴏɢᴏ ɪs ʙᴀsᴇᴅ ᴏɴ ᴜʙ ᴀɴᴅ ᴍᴀᴅᴇ ʙʏ [IronMen ʜᴇʟᴘ](https://t.me/Ironmenfed)",
             force_document=False,
         )
         os.remove(flnme)
@@ -212,18 +211,186 @@ async def logo_(event):
     if os.path.exists(bg_):
         os.remove(bg_) 
     if os.path.exists(font_):
-        if not font_.startswith("./InsaneRobot/resources/fonts"):
+        if not font_.startswith("./IronMenRobot/resources/fonts"):
             os.remove(font_)
             
+            
+            
+            
+# Logo Fir Girl
+@register(pattern="^/glogo ?(.*)")
+async def logo_gen(event):
+    xx = await event.reply("`Preparing your logo...`")
+    name = event.pattern_match.group(1)
+    if not name:
+        await xx.edit("`Provide some text to draw!\nExample: /logo <your name>!`")
+        return
+    bg_, font_ = "", ""
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    font_ = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    font_ = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
+    else:
+        pics = []
+        async for i in ubot2.iter_messages(
+            "@alexagirllogopack", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if not bg_:
+        pics = []
+        async for i in ubot2.iter_messages(
+            "@alexagirllogopack", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+    if not font_:
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        fnt_size = 120
+        strke = 10
+    elif len(name) >= 9:
+        fnt_size = 50
+        strke = 5
+    else:
+        fnt_size = 100
+        strke = 20
+    img = Image.open(bg_)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
+    h += int(h * 0.21)
+    image_width, image_height = img.size
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        name,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
+    draw.text((x, y), name, font=font, fill="white",
+              stroke_width=strke, stroke_fill="black")
+    flnme = f"logo.png"
+    img.save(flnme, "png")
+    await xx.edit("`Uploading`")
+    if os.path.exists(flnme):
+        await tbot.send_file(
+            event.chat_id,
+            file=flnme,
+            caption="ᴛʜɪs ʟᴏɢᴏ ɪs ʙᴀsᴇᴅ ᴏɴ ᴜʙ ᴀɴᴅ ᴍᴀᴅᴇ ʙʏ [IronMen ʜᴇʟᴘ](https://t.me/IronMenfed)",
+            force_document=False,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_) 
+    if os.path.exists(font_):
+        if not font_.startswith("./IronMenRobot/resources/fonts"):
+            os.remove(font_)
 
-__mod_name__ = "©️ ʟᴏɢᴏᴍᴀᴋᴇʀ"
+
+@register(pattern="^/gwlogo ?(.*)")
+async def logo_(event):
+    xx = await event.reply("`Preparing your logo...`")
+    name = event.pattern_match.group(1)
+    if not name:
+        await xx.edit("`Provide some text to draw!\nExample: /wlogo <your name>!`")
+        return
+    bg_, font_ = "", ""
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    font_ = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    font_ = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
+    else:
+        pics = []
+        async for i in ubot2.iter_messages(
+            "@alexagirllogopack", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if not bg_:
+        pics = []
+        async for i in ubot2.iter_messages(
+            "@alexagirllogopack", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+    if not font_:
+        fpath_ = glob.glob("./IronMenRobot/resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        fnt_size = 105
+        strke = 8
+    elif len(name) >= 9:
+        fnt_size = 50
+        strke = 4
+    else:
+        fnt_size = 95
+        strke = 13
+    img = Image.open(bg_)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
+    h += int(h * 0.21)
+    image_width, image_height = img.size
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        name,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
+    draw.text((x, y), name, font=font, fill="white",
+              stroke_width=strke, stroke_fill="black")
+    flnme = f"logo.png"
+    img.save(flnme, "png")
+    await xx.edit("`Uploading`")
+    if os.path.exists(flnme):
+        await tbot.send_file(
+            event.chat_id,
+            file=flnme,
+            caption="ᴛʜɪs ʟᴏɢᴏ ɪs ʙᴀsᴇᴅ ᴏɴ ᴜʙ ᴀɴᴅ ᴍᴀᴅᴇ ʙʏ [Insane ʜᴇʟᴘ](https://t.me/IronMenfed)",
+            force_document=False,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_) 
+    if os.path.exists(font_):
+        if not font_.startswith("./IronMenRobot/resources/fonts"):
+            os.remove(font_)
+
+
+__mod_name__ = "👩‍🎨 ʟᴏɢᴏᴍᴀᴋᴇʀ"
 
 __help__ = """ This is help menu for logomaker
-
-❂ /logo <text/name> - Create a logo with random view.
-❂ /wlogo <text/name> - Create a logo with wide view only.
-
+❂ /blogo <text/name> - Create a logo with random view for boys.
+❂ /bwlogo <text/name> - Create a logo with wide view only for boys.
+❂ /glogo <text/name> - Create a logo with random view for girls.
+❂ /gwlogo <text/name> - Create a logo with wide view only for girls.
  Image Editor :
-
 ❂  /edit <reply photo> - to edit image.
 """
