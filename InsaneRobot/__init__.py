90a2a1f35665486310c20d4800c9b5791197139e
Devil3420 <105910403+Devil3420@users.noreply.github.com>
2022-07-08 18:35:12 +0530

    Update __init__.py

--- InsaneRobot/__init__.py
+++ InsaneRobot/__init__.py
@@ -130,9 +130,7 @@ if ENV:
         raise Exception(
             "Your blacklisted chats list does not contain valid integers.")
 
-else:
-    from InsaneRobot.config import Development as Config
-    TOKEN = Config.TOKEN
+
 
     try:
         OWNER_ID = int(Config.OWNER_ID)
